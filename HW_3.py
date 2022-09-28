ec7bde8427dd4cc89f8db4e2621c2e6f0eb3e6d9
brandonalston <bca3@rice.edu>
2022-09-28 02:06:40 -0500

    HW3

--- HW_3.py
+++ HW_3.py
@@ -66,7 +66,6 @@ def SSQ(T:float):
 
 
 def problem_1():
-
     A, D, S, idle, over = SSQ(9)
     plt.plot(S.keys(), S.values())
     plt.xlabel("time")
@@ -92,8 +91,7 @@ def problem_1():
 
 
 def PP_lambda(t):
-    val = 19 - 3*abs((t % 10) - 5)
-    return val
+    return 19 - 3*abs((t % 10) - 5)
 
 
 def next_arrival(t):
@@ -108,10 +106,11 @@ def NH_SSQ(T):
     t_A = next_arrival(0)
     t_B, t_D = 0.3*random.random(), math.inf
     t, N_A, N_D = 0, 0, 0
-    N_B, K = 1, 1  # assume start on break -> event at beginning
-    A, D, B_L, B_R, S, E, J = {}, {}, {}, {}, {}, {}, {}  # dicts to record event times/number of each event
+    A, D, B_L, B_W, E, S, J = {}, {}, {}, {}, {}, {}, {}  # dicts to record event times/number of each event
+    N_B, K = 1, 1  # assume start on break
     S[K], E[K], J[K] = 0, 0, 0
-    while min([t_A, t_B, t_D]) <= T:
+    B_L[N_B] = 0
+    while min(t_A, t_D, t_B) <= T:
         K += 1
         # customer arrives as event
         if t_A < t_D and t_A < t_B:
@@ -123,14 +122,15 @@ def NH_SSQ(T):
             J[K] = J[K-1]
             # update next arrival/departure times
             t_A = next_arrival(t)
-            # server on break
+            # server working
             if S[K] > 1:
-               t_D = t_D
-            if J[K] == 0:
+                t_D = t_D
+            elif S[K] == 1 and J[K] == 1:
+                t_D = t-(1/25)*math.log(random.random())
+            # server not working
+            elif J[K] == 0:
                 t_D = math.inf
-            elif J[K] == 1:
-                if S[K] == 1:
-                    t_D = t-(1/25)*math.log(random.random())
+
         # complete job as event
         elif t_D < t_A and t_D < t_B:
             t = t_D
@@ -138,18 +138,18 @@ def NH_SSQ(T):
             E[K] = t
             S[K] = S[K-1]-1
             # update server break position
-            if S[K] == 0:
-                J[K] = 0
-            elif S[K] > 0:
+            if S[K] > 0:
                 J[K] = 1
+            elif S[K] == 0:
+                J[K] = 0
             # update t_B and t_D
             if S[K] == 0:
                 t_D = math.inf
             elif S[K] > 0:
-                t_D = t-(1/25)*math.log(random.random())
+                t_D = t - (1/25)*math.log(random.random())
             if J[K] == 0:
-                t_B += (t+0.3*random.random())
-            else:
+                t_B = t+0.3*random.random()
+            elif J[K] == 1:
                 t_B = math.inf
             # store departure event data
             D[N_D] = t
@@ -159,12 +159,12 @@ def NH_SSQ(T):
         # return from break as event
         elif t_B < t_A and t_B < t_D:
             t = t_B
-            B_R[N_B] = t
+            B_W[N_B] = t
             E[K] = t
             S[K] = S[K-1]
             if S[K] > 0:
                 J[K] = 1
-            if S[K] == 0:
+            elif S[K] == 0:
                 J[K] = 0
             if S[K] == 0:
                 N_B += 1
@@ -172,21 +172,103 @@ def NH_SSQ(T):
             # update t_B and t_D
             if S[K] == 0:
                 t_D = math.inf
-            if S[K] > 0:
+            elif S[K] > 0:
                 t_D = t-(1/25)*math.log(random.random())
             if S[K] == 0:
                 t_B = t + 0.3*random.random()
-            if J[K] == 0:
+            elif J[K] == 1:
                 t_B = math.inf
     if J[K] == 0:
-        B_R[N_B] = T
-    return A, D, B_L, B_R, S, E, J, K
+        B_W[N_B] = T
+    return A, D, B_L, B_W, S, E, J, K
 
 
 def problem_2():
-    A, D, B_L, B_R, S, E, J, K = NH_SSQ(10)
-    print(A)
-    print(D)
+    m = 500
+    break_time = 0
+    for i in range(m):
+        A, D, B_L, B_W, S, E, J, K = NH_SSQ(100)
+        time_on_break = {number: B_W[number]-B_L[number] for number in B_L}
+        break_time += sum(list(time_on_break.values()))
+    break_time /= m
+    print('Avg time on break:', round(break_time,4))
     return
+
+
+def poisson(T, lamda):
+    Ta = []
+    t = -math.log(random.random()) / lamda
+    while t < T:
+        Ta.append(t)
+        t = t -math.log(random.random()) / lamda
+    return Ta
+
+
+def CD_SSQ(T, lamda):
+    arrival_times = poisson(T, lamda)  # arrival time
+    n = len(arrival_times)
+    server_times = np.zeros(n)  # Server Start time
+    departure_times = np.zeros(n)  # Depart time
+    idle_times = np.zeros(n)  # Idle time
+    wait_times = np.zeros(n)  # Wait time
+
+    server_times[0] = arrival_times[0]
+    G = -math.log(random.random())/4
+    departure_times[0] = server_times[0] + G
+    idle_times[0] = arrival_times[0]
+    wait_times[0] = 0
+
+    jobs_leaving = np.zeros(n)
+    for iter in range(1,n):
+        server_times[iter] = max(departure_times[iter-1], arrival_times[iter])
+        wait_times[iter] = server_times[iter] - arrival_times[iter]
+        F = 5 * random.random()
+        if wait_times[iter] > F:
+            departure_times[iter] = departure_times[iter - 1]
+            jobs_leaving[iter] = 1
+        else:
+            G = -math.log(random.random())/4
+            departure_times[iter] = server_times[iter] + G
+    number_left = sum(jobs_leaving)
+    return number_left
+
+
+def problem_3():
+    m = 500
+    avgleft = 0
+
+    for iter in range(m):
+        avgleft = avgleft + CD_SSQ(100, 5)
+
+    avgleft = avgleft / m
+    print("Expected number of lost customers:", avgleft)
+    return
+
+
+def insurance_claim(T, lamda):
+    t = 0
+    M = 25000
+    while t < T:
+        ta = - math.log(random.random()) / lamda
+        t = t + ta
+        claim = -1000*math.log(random.random())
+        payment = ta*11000
+        M = M + payment - claim
+        if M < 0:
+            return 0
+    return 1
+
+
+def problem_4():
+    m = 1000
+    prob_good = 0
+    for iter in range(m):
+        prob_good += insurance_claim(365, 10)
+    prob_good /= m
+    print("Probability always positive:", prob_good)
+
+
 # problem_1()
-problem_2()
+# problem_2()
+# problem_3()
+problem_4()
